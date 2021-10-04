65b842e4ad5f9e2bba7d6c20b0056847c5716574
calexandar <alexandercvet243@gmail.com>
2021-10-04 15:38:57 +0200

    initial_commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+<header class="text-center bg-blue-800 text-white p-4 mb-10">
+  <div class="text-3xl md:text-5xl mb-3"><i class="fas fa-viruses"></i>Covid-19 Tracker</div>
+  <p>Made by <span  class="text-blue-300"> Alex</span></p>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
